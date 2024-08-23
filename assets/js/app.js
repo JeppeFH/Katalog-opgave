5fc60e3cbc6ad4c19c01e185b605c2526efd7e3f
JeppeFH <jeppefinch@hotmail.dk>
2024-08-23 15:21:37 +0200

    update img + html + json

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,3 +8,20 @@ app.init = async () => {
 };
 
 app.init();
+
+/*Navigation*/
+let burgerMenuOpen = document.querySelector(".burgerMenuOpen");
+let burgerMenuClose = document.querySelector(".burgerMenuClose");
+let navLinks = document.querySelector(".nav-links");
+
+burgerMenuOpen.addEventListener("click", () => {
+  navLinks.classList.add("active");
+  burgerMenuOpen.classList.add("active");
+  burgerMenuClose.classList.add("active");
+});
+
+burgerMenuClose.addEventListener("click", () => {
+  navLinks.classList.remove("active");
+  burgerMenuOpen.classList.remove("active");
+  burgerMenuClose.classList.remove("active");
+});
