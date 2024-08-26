0fc901427f9646d969648fa9d67cbf7667f04110
JeppeFH <jeppefinch@hotmail.dk>
2024-08-26 09:26:57 +0200

    update img

--- assets/js/templates.js
+++ assets/js/templates.js
@@ -1,21 +1,21 @@
 // Template functions - hver function opfører sig som en HTML template.
-
 export const playerListTmpl = (player) =>
   `
-          <div class="product-list-display">
-          <img src="${player.image}${player.id}">
-          <p><b>${player.title}</b></p>
-          <p><i>${player.price} kr</i></p>
-          <label class="read-more-btn" ><a>Læs mere om produkt</a></label>
-          <div class="read-more">  
-          <p>${player.info1}</p>
-           <p>${player.info2}</p>
-            <p>${player.info3}</p>
-          </div>
-      <button class="product-btn"><a href="page.html?id=${player.id}">Se produkt</a></button> 
-       
-      </div>
-      `;
+      <img src="${player.picture}${player.id}">
+                <div><p>${player.ranking}</p></div>
+                <div><p>${player.name}</p></div>
+                <p><b>${player.title}</b></p>
+                <p><i>${player.price} kr</i></p>
+                <label class="read-more-btn" ><a>Læs mere om produkt</a></label>
+                <div class="read-more">  
+                <p>${player.info1}</p>
+                 <p>${player.info2}</p>
+                  <p>${player.info3}</p>
+                </div>
+            <button class="product-btn"><a href="page.html?id=${player.id}">Se produkt</a></button> 
+             
+            </div>
+            `;
 
 export const playerPageTmpl = (player) =>
   `
